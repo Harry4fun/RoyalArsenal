5e7d1f7d10ca28dc0cf6f7dec9e8aae7bd179af0
Harry4fun <jiaojingkuan123@163.com>
2018-09-06 00:06:49 +0800

    init
    Signed-off-by: Harry4fun <jiaojingkuan123@163.com>

---
+++ app.js
@@ -0,0 +1,9 @@
+App({//生命周期
+  onLaunch: function () { },//监听初始化
+  onShow: function () { },//监听显示（进入前台）
+  onHide: function () { },//监听隐藏（进入后台：按home离开微信）
+  onError: function (msg) { },//监听错误
+  //自定义的全局方法和全局变量  
+  globalFun: function () { },
+  globalData: 'I am global data'
+})
